b957bb43459443fcb510ad83a026b19d8fb024f8
rydvone <rydvone@gmail.com>
2022-05-21 15:46:30 +0300

    feat: 01-read-file is done

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,1 +1,11 @@
- 
+const fs = require('fs');
+const path = require('path');
+const { stdout } = process;
+const inputStream = fs.createReadStream(
+  path.join(__dirname, 'text.txt'),
+  'utf-8');
+
+inputStream.on('data', (ch) => stdout.write(ch));
+// inputStream.on('end', () => console.log('the end'));
+inputStream.on('error', (err) => console.log(err.message));
+
