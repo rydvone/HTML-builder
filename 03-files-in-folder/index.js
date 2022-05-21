7af5e9e304b5064cf9ce83d9b10ef82c954b4115
rydvone <rydvone@gmail.com>
2022-05-22 02:21:44 +0300

    feat: 03-files-in-folder is done

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const process = require('process');
+const fs = require('fs');
+
+const folderPath = path.join(__dirname, 'secret-folder');
+fs.readdir(folderPath,
+  { withFileTypes: true },
+  (err, files) => {
+    if (err) console.log(err.message);
+    let arrFiles = files.filter(el => el.isFile());
+    arrFiles.forEach(el => {
+     
+      let res = [];
+      fs.stat(path.join(__dirname, 'secret-folder', el.name), (err, stats) => {
+        let resExtnameIn = path.extname(el.name);
+        let resNameIn = path.basename(el.name);
+        let resSizeIn = stats.size;
+        res.push(resNameIn.slice(0, (resNameIn.length - resExtnameIn.length)));
+        res.push(resExtnameIn.slice(1));
+        res.push(stats.size);
+        console.log(res.join(' - ') + 'b');
+      });
+    });
+
+    console.log(arrFiles);
+
+  });
+
+
+// if (true) {
+//   process.exit();
+// }
+
+// process.on('exit', () => console.log('\ngoodbuy everybody\n'));
+
+
+
