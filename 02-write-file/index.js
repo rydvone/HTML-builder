d16ba01ea5d9998a296e495eb8f271e7ad25976f
rydvone <rydvone@gmail.com>
2022-05-22 22:19:24 +0300

    refactor: clean code 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,10 +1,9 @@
 const path = require('path');
 const fs = require('fs');
 const process = require('process');
-const { stdin, stdout, exit } = process;
+const { stdout} = process;
 const readline = require('readline');
-// const EventEmitter = require('events');
-// const em = new EventEmitter();
+
 let rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
@@ -12,23 +11,17 @@ let rl = readline.createInterface({
 
 const currentPath = path.join(__dirname, 'notes.txt');
 const outStream = fs.createWriteStream(currentPath);
-// const inStream = fs.createReadStream();
 
-stdout.write('\nHello, friend!\nPS: Write your text below. Dont forget press Ctrl+C or write <exit> to the end your message\n');
-let i = 1;
+stdout.write('\nHello, friend!\nPS: Write your text below. Dont forget press Ctrl+C or write <exit>+press<Enter> to exit\n');
+
 rl.on('line', (line) => {
-  let arrTemp = line.split(' ');
-  let findExit = arrTemp.includes('exit');
-  
-  if (findExit) {
+
+  if (line.trim() === 'exit') {
     rl.close();
-
   }
-
-  outStream.write(line + '\n')
-  console.log('line-' + i + ': ' + line + '\n');
-  i++;
+  outStream.write(line + '\n');
 });
 
 rl.on('close', () => process.exit(0));
 process.on('exit', () => console.log('\nGoodbuy, friend.'));
+
