7593cde6610958fffdd8a60306cc52c601595bd9
rydvone <rydvone@gmail.com>
2022-05-23 01:09:30 +0300

    feat: start 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -2,5 +2,26 @@ const path = require('path');
 const fs = require('fs');
 const fsPromises = require('fs/promises');
 
-const pathStyles = path.join(__dirname, 'styles');
-const pathBundle = path.join(__dirname, 'project-dist', 'bundle.css');
+const pathTemplate = path.join(__dirname, 'template.html');
+const pathIndexHtml = path.join(__dirname, 'project-dist', 'index.html');
+const pathStyleCss = path.join(__dirname, 'project-dist', 'style.css');
+
+
+let dataTemplate = '';
+
+const readTemplate = (path) => {
+  let dataTemp = '';
+  const rs = fs.createReadStream(path, 'utf-8');
+
+  rs.on('data', (chunk) => dataTemp += chunk);
+  rs.on('end', () => console.log(dataTemp));
+
+  // let finder = dataTemp.match(/{{.*}}/);
+  // console.log(finder);
+  console.log(dataTemp);
+  return dataTemp;
+};
+
+console.log(readTemplate(pathTemplate));
+// /{{.*}}/g
+// '12-34-56'.replace( /-/g, ":" )
