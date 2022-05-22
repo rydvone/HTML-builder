a0e50c97d7948ad44fc556415737638f80a108fa
rydvone <rydvone@gmail.com>
2022-05-22 20:55:34 +0300

    feat: add forEach

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const process = require('process');
 const fs = require('fs');
 const fsPromises = require('fs/promises');
 
@@ -7,46 +6,36 @@ const pathStyles = path.join(__dirname, 'styles');
 const pathBundle = path.join(__dirname, 'project-dist', 'bundle.css');
 
 
-const delFromFilesCopy = async (path) => {
-  await fsPromises.rm(path);
-};
-
-const copyAllFiles = async (path, pathCopy) => {
-  await fsPromises.copyFile(path, pathCopy);
-};
-
 const getFiles = async (path) => {
   return await fsPromises.readdir(path, { withFileTypes: true });
 };
 
-async function workDir() {
+const copyData = async (files) => {
+  const ws = fs.createWriteStream(pathBundle);
 
+  files.forEach((el) => {
+    const rs = fs.createReadStream( path.join(__dirname, 'styles', el), 'utf-8');
+    let dataTemp = '';
+    rs.on('data', chunk => dataTemp += chunk);
+    rs.on('end', () => 
+      fs.appendFile(pathBundle, 
+        dataTemp, 
+        (err) => {if (err) console.log(err.message)}));
+    rs.on('error', error => console.log('Error', error.message));
+  });
+};
+
+
+async function workDir() {
   let curFiles = await getFiles(pathStyles);
-  // let resExtnameIn = path.extname(el.name);
 
   console.log('curFilesCopy       1 =     ');
   console.log(curFiles);
   let cssFiles = curFiles.filter((el) => el.isFile() && path.extname(el.name) === '.css');
+  // get array with name only css files
   cssFiles = cssFiles.map((el) => el.name);
   console.log(cssFiles);
-  // curFilesCopy.forEach((el) => {
-  //   let pathDel = path.join(__dirname, 'files-copy', el);
-  //   delFromFilesCopy(pathDel);
-  // });
-
-  // await delFromFilesCopy(curFilesCopy);
-
-  // curFilesCopy = await getFiles(pathFolderCopy);
-  // console.log('curFilesCopy after del =   ' + curFilesCopy);
-
-  // const curFiles = await getFiles(pathFolder);
-  // curFiles.forEach((el) => {
-  //   let pathSrc = path.join(__dirname, 'files', el);
-  //   let pathDest = path.join(__dirname, 'files-copy', el);
-  //   copyAllFiles(pathSrc, pathDest);
-  // });
-  // curFilesCopy = await getFiles(pathFolderCopy);
-  // console.log('curFilesCopy final =   ' + curFilesCopy);
+  await copyData(cssFiles);
 }
 
 try {
