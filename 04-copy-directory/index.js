b60255b5af12bee168089ac5998f6133f3e323e5
rydvone <rydvone@gmail.com>
2022-05-23 22:21:46 +0300

    feat: copy and delete w recursive

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,6 +4,10 @@ const fsPromises = require('fs/promises');
 const pathFolder = path.join(__dirname, 'files');
 const pathFolderCopy = path.join(__dirname, 'files-copy');
 
+const getFiles = async (path) => {
+  return await fsPromises.readdir(path, { withFileTypes: true });
+};
+
 const makeDir = async (path) => {
   await fsPromises.mkdir(
     path,
@@ -14,38 +18,61 @@ const makeDir = async (path) => {
   );
 };
 
-const delFromFilesCopy = async (path) => {
-  await fsPromises.rm(path, { recursive: true }, (err) => {if (err) console.log(err.message);});
+const delFiles = async (pth) => {
+  let curList = await fsPromises.readdir(pth);
+  for (const el of curList) {
+    let checkIsFile = (await fsPromises.lstat(path.join(pth, el))).isFile();
+    if (checkIsFile) {
+      await fsPromises.rm(path.join(pth, el), {recursive: true});
+    } else {
+      await delFiles(path.join(pth, el));
+    }
+  }
 };
 
-const copyAllFiles = async (path, pathCopy) => {
+const delFolders = async (pth) => {
+  await fsPromises.rm(pth, { recursive: true });
+};
+
+const delFoldersFiles = async (pth) => {
+  let curFiles = await getFiles(__dirname);
+  let findDist = curFiles.find(
+    (el) => el.isDirectory() && el.name === 'files-copy'
+  );
+  if (findDist) {
+    await delFiles(pth);
+    await delFolders(pth);
+  }
+};
+
+const copyCurFile = async (path, pathCopy) => {
   await fsPromises.copyFile(path, pathCopy);
 };
 
-const getFiles = async (path) => {
-  return await fsPromises.readdir(path);
+const copyFolder = async (from, to) => {
+  await makeDir(to);
+  let curList = await fsPromises.readdir(from);
+  for (const el of curList) {
+    let checkIsFile = (await fsPromises.lstat(path.join(from, el))).isFile();
+    if (checkIsFile) {
+      await copyCurFile(path.join(from, el), path.join(to, el));
+    } else {
+      await copyFolder(path.join(from, el), path.join(to, el));
+    }
+  }
 };
 
-async function workDir() {
-  await makeDir(pathFolderCopy);
-  let curFilesCopy = await getFiles(pathFolderCopy);
 
-  for (const el of curFilesCopy) {
-    let pathDel = path.join(__dirname, 'files-copy', el);
-    await delFromFilesCopy(pathDel);
-  }
-
-  const curFiles = await getFiles(pathFolder);
-  for (const el of curFiles) {
-    let pathSrc = path.join(__dirname, 'files', el);
-    let pathDest = path.join(__dirname, 'files-copy', el);
-    await copyAllFiles(pathSrc, pathDest);
-  }
+async function mainFn() {
+  try {
+    await delFoldersFiles(pathFolderCopy);
+    await makeDir(pathFolderCopy);
+    await copyFolder(pathFolder, pathFolderCopy);    
+  } catch (error) {
+    console.log(error.message);
+  }  
 }
 
-try {
-  workDir();
-} catch (error) {
-  console.log(error.message);
-}
+mainFn();
+
 
