9019164478b7a7aabd767c7451023637b5ba8993
rydvone <rydvone@gmail.com>
2022-05-23 21:17:21 +0300

    feat: #06 is work

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -3,33 +3,13 @@ const fs = require('fs');
 const fsPromises = require('fs/promises');
 
 const pathDist = path.join(__dirname, 'project-dist');
+const pathDistAssets = path.join(__dirname, 'project-dist', 'assets');
 const pathComponents = path.join(__dirname, 'components');
 const pathStyles = path.join(__dirname, 'styles');
 const pathAssets = path.join(__dirname, 'assets');
 const pathTemplate = path.join(__dirname, 'template.html');
 const pathIndexHtml = path.join(__dirname, 'project-dist', 'index.html');
 const pathStyleCss = path.join(__dirname, 'project-dist', 'style.css');
-
-// let dataTemplate = '';
-
-// const readTemplate = (path) => {
-//   let dataTemp = '';
-//   const rs = fs.createReadStream(path, 'utf-8');
-
-//   rs.on('data', (chunk) => dataTemp += chunk);
-//   rs.on('end', () => console.log(dataTemp));
-
-//   // let finder = dataTemp.match(/{{.*}}/);
-//   // console.log(finder);
-//   console.log(dataTemp);
-//   return dataTemp;
-// };
-
-// console.log(readTemplate(pathTemplate));
-// /{{.*}}/g
-// '12-34-56'.replace( /-/g, ':' )
-
-
 
 const makeDir = async (path) => {
   await fsPromises.mkdir(
@@ -41,7 +21,6 @@ const makeDir = async (path) => {
   );
 };
 
-// get list file
 const getFiles = async (path) => {
   return await fsPromises.readdir(path, { withFileTypes: true });
 };
@@ -53,19 +32,11 @@ const getComponentsData = async (files) => {
       path.join(__dirname, 'components', el),
       'utf-8'
     );
-    // const chunks = [];
     let dataTemp = '';
     for await (const chunk of rs) {
-      // chunks.push(chunk);
       dataTemp += chunk;
-      // console.log(chunk);
     }
     objComponentsData[el.slice(0, -5)] = dataTemp;
-    // if (el.slice(0, -5) == 'header') {
-    //   fs.appendFile(path.join(__dirname, 'components', 'el.html'),
-    //     dataTemp,
-    //     (err) => {if (err) console.log(err.message)});
-    // }
   }
   return objComponentsData;
 };
@@ -108,69 +79,60 @@ const mergeCss = async (files) => {
   });
 };
 
-// const copyFiles = async (path1) => {
-//   fs.readdir(path1, (err, files) => {
-//     if(err) console.log('error:', err.message);
-//     for (let file of files){
-//        fs.stat(path.join(__dirname, file),
-//         (errStat, status) => {
-//           if(errStat) console.log('error:', errStat.message);
-
-//           if(status.isFile()){
-//              console.log('Папка: ' + file);
-//              copyFiles(path1 + '/' + file);
-//           }else{
-//              console.log('Файл: ' + file);
-//           }
-//        });
-//     }
-//  });
-// };
 
 const copyCurFile = async (path, pathCopy) => {
   await fsPromises.copyFile(path, pathCopy);
 };
 
-
 const copyFolder = async (from, to) => {
   await makeDir(to);
   let curList = await fsPromises.readdir(from);
-
   for (const el of curList) {
     let checkIsFile = (await fsPromises.lstat(path.join(from, el))).isFile();
     if (checkIsFile) {
-      console.log('file');
       await copyCurFile(path.join(from, el), path.join(to, el));
     } else {
-      console.log('not file');
       await copyFolder(path.join(from, el), path.join(to, el));
     }
   }
-
-
-
-  // if (fs.stat(path.join(from, el)).isFile()) {
-  //   // fs.copyFile(path.join(from, element), path.join(to, element));
-  //   console.log('is file');
-  // } else {
-  //   await copyFolder(path.join(from, el), path.join(to, el));
-  // }
-
-
-  return curList;
 };
 
-const delFolderFile = async (pth) => {
-  await fsPromises.rmdir(pth, { recursive: true, force: true }, ((err) => {
-    if (err) throw err;
-  }));
+const delFiles = async (pth) => {
+  let curList = await fsPromises.readdir(pth);
+  for (const el of curList) {
+    let checkIsFile = (await fsPromises.lstat(path.join(pth, el))).isFile();
+    if (checkIsFile) {
+      await fsPromises.rm(path.join(pth, el));
+    } else {
+      await delFiles(path.join(pth, el));
+    }
+  }
 };
 
-async function workDir() {
+const delFolders = async (pth) => {
+  await fsPromises.rm(pth, { recursive: true });
+};
+
+const delFoldersFiles = async (pth) => {
+  let curFiles = await getFiles(__dirname);
+  let findDist = curFiles.find(
+    (el) => el.isDirectory() && el.name === 'project-dist'
+  );
+  if (findDist) {
+    await delFiles(pth);
+    await delFolders(pth);
+  }
+};
+
+
+async function mainFn() {
   try {
-    await makeDir(pathDist);
-    let curFiles = await getFiles(pathComponents);
+    // delete project-dist before new build
+    await delFoldersFiles(pathDist);
 
+    await makeDir(pathDist);
+    
+    let curFiles = await getFiles(pathComponents);
     // get object w content html components
     let componentsFiles = curFiles.filter(
       (el) => el.isFile() && path.extname(el.name) === '.html'
@@ -184,7 +146,7 @@ async function workDir() {
 
     // make data w content html template + components
     let indexHtmlData = await makeIndexHtmlData(templateData, componentsData);
-
+    
     // write data html to file
     await writeIndexHtml(indexHtmlData, pathIndexHtml);
 
@@ -192,25 +154,19 @@ async function workDir() {
     curFiles = await getFiles(pathStyles);
     let cssFiles = curFiles.filter((el) => el.isFile() && path.extname(el.name) === '.css');
     cssFiles = cssFiles.map((el) => el.name);
-    console.log(cssFiles);
     // merge css files 
     await mergeCss(cssFiles);
 
     const pathFrom = path.join(__dirname, 'assets');
     const pathTo = path.join(__dirname, 'project-dist', 'assets');
 
-    // await copyFiles(pathAssets);
-
     // copy assets to project-dist
-    // let someth = await copyFolder(pathFrom, pathTo);
-    // console.log(someth);
+    await copyFolder(pathFrom, pathTo);
 
-
-    await delFolderFile(pathDist);
   } catch (error) {
     console.log('error is: ' + error.message);
   }
 }
 
-workDir();
+mainFn();
 
